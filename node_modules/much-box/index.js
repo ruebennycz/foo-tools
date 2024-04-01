e4b9cc5aac28770798621a635c6504723ac87af7
ruebennycz <ruebennycz@gmail.com>
2024-04-01 23:27:44 +0800

    change code

---
+++ node_modules/much-box/index.js
@@ -0,0 +1,13 @@
+
+const axios = require("axios");
+
+console.log("hello world");
+
+
+axios.get("https://api.example.com/data")
+  .then(response => {
+    console.log(response.data);
+  })
+  .catch(error => {
+    console.error(error);
+  });
