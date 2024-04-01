a7096622e93bcf9232395e76e67b7c986c81d98a
ruebennycz <ruebennycz@gmail.com>
2024-04-02 00:43:54 +0800

    version update

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 const axios = require("axios");
 const async = require("async");
@@ -12,18 +11,3 @@ console.log("hello world");
 
 
 
-=======
-
-const axios = require("axios");
-const async = require("async");
-const bcrypt = require("bcrypt");
-
-console.log("hello world");
-console.log("hello world");
-console.log("hello world");
-console.log("hello world");
-console.log("hello world");
-
-
-
->>>>>>> c06a553 (foo-tools update)
